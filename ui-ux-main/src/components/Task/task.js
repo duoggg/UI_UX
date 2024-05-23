bb5c47818346d07a3e422fe048badee46118c62f
trang20210852 <125128489+trang20210852@users.noreply.github.com>
2024-05-24 02:54:33 +0700

    upload responsive

--- ui-ux-main/src/components/Task/task.js
+++ ui-ux-main/src/components/Task/task.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Sider from '../Sider';
 import Header from '../Header';
 import './task.css';
+import './taskResponsive.css';
 import CreateGoal from './Create/create';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faAngleRight, faAngleLeft} from '@fortawesome/free-solid-svg-icons'
